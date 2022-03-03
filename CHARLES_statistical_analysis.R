570d3140d17000df7f391bb5a1d092b996992b8a
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-03 13:43:50 -0800

    finished analysis for movie overview tf-idf

--- CHARLES_statistical_analysis.R
+++ CHARLES_statistical_analysis.R
@@ -5,7 +5,8 @@ library(datarium)
 
 getwd()
 
-iq <- read.csv("./out.csv")
+# iq <- read.csv("./out.csv")
+iq <- read.csv("./out_windowed_mean.csv")
 
 # Run linear regression
 m1 = lm(TF.IDF.Total ~ Popularity, data=iq)
@@ -23,17 +24,61 @@ plot(m1)
 # Check summary of regression
 summary(m1)
 
-# Log transform TF.IDF.Total to solve for non-constant variance
-iq$LOG.TF.IDF.Total <- log10(iq$TF.IDF.Total + 0.0000001)
+# MODEL 3 (THIS IS OUR BEST MODEL THAT WE CHOSE)
 
-# Run linear regression with log transformed TF.IDF.Total
-m2 = lm(LOG.TF.IDF.Total ~ Popularity, data=iq)
+# Run polynomial regression 
+m3 = lm(Popularity ~ TF.IDF.Total + I(TF.IDF.Total^2), data=iq)
 
 # Check constant variance assumption of the regression model.
 # Check linearity assumption of the regression model
 # Check normality assumption of the linear regression model
-plot(m2)
+plot(m3)
 
 # Check summary of regression
-summary(m2)
+# probability of F that large under the null hypothesis of no effect is small, reject null hypothesis
+summary(m3)
+
+# Anova table to see if 
+anova(m3)
+
+
+#plot x vs. y
+plot(iq$TF.IDF.Total, iq$Popularity, pch=16, cex=1.5) 
+#use model to get predicted values
+pred <- predict(m3)
+ix <- sort(iq$TF.IDF.Total, index.return=T)$ix
+#add polynomial curve to plot
+lines(iq$TF.IDF.Total[ix], pred[ix], col='red', lwd=2)
+
+# MODEL 4
+iq$LOG_Popularity <- log10(iq$Popularity)
+
+# Run polynomial regression on the log of popularity
+m4 = lm(LOG_Popularity ~ TF.IDF.Total + I(TF.IDF.Total^2), data=iq)
+
+# Check constant variance assumption of the regression model.
+# Check linearity assumption of the regression model
+# Check normality assumption of the linear regression model
+plot(m4)
+
+# Check summary of regression
+# probability of F that large under the null hypothesis of no effect is small, reject null hypothesis
+summary(m4)
+
+# Anova table to see if 
+anova(m4)
+
+
+#plot x vs. y
+plot(iq$TF.IDF.Total, iq$LOG_Popularity, pch=16, cex=1.5) 
+#use model to get predicted values
+pred <- predict(m4)
+ix <- sort(iq$TF.IDF.Total, index.return=T)$ix
+#add polynomial curve to plot
+lines(iq$TF.IDF.Total[ix], pred[ix], col='red', lwd=2)
+
+
+
+
+
 
